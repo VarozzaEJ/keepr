3e88d62733283035d9ac5adda775c0b5b20668d6
Evan Varozza <VarozzaEJ@gmail.com>
2024-08-28 14:17:17 -0600

    deploy

--- client/src/env.js
+++ client/src/env.js
@@ -1,7 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 
 // REVIEW don't forget to change this URL if you're using a dotnet backend
-export const baseURL = dev ? 'https://localhost:7045' : 'https://evanvarozza.dev'
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-cucsliboqrmpiibf.us.auth0.com'
 export const clientId = '9IHCopOWsypgrZC9Nniolr8lpzQ6Xvig'
